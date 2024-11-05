dad9fdaca8d02658976b6d5f9cde18af80b56cd7
sam <sam@meech-ward.me>
2024-11-05 12:59:40 -0800

    allow rds iam connection

---
+++ server/db/iam-connect.ts
@@ -0,0 +1,27 @@
+import { Signer } from "@aws-sdk/rds-signer";
+import mysql from "mysql2/promise";
+
+export async function generateIamToken({host, user, region}: {host: string, user: string, region: string}) {
+  const signer = new Signer({
+    hostname: host,
+    port: 3306,
+    username: user,
+    region,
+  });
+
+  return await signer.getAuthToken();
+}
+
+export async function connectWithIamToken({host, user, token, database}: {host: string, user: string, token: string, database: string}) {
+  const connection = await mysql.createPool({
+    host,
+    user,
+    password: token,
+    database,
+    insecureAuth: true,
+    ssl: 'Amazon RDS',
+  });
+
+  return connection;
+}
+
--- server/db/index.ts
+++ server/db/index.ts
@@ -1,12 +1,8 @@
 import { drizzle } from "drizzle-orm/mysql2";
-import mysql from "mysql2/promise";
 import { z } from "zod";
 import { logger } from "../lib/logger";
 import { type Logger } from "drizzle-orm/logger";
-
-const DbEnv = z.object({
-  MYSQL_URL: z.string().url(),
-});
+import { connectWithIamToken, generateIamToken } from "./iam-connect";
 
 class MyLogger implements Logger {
   logQuery(query: string, params: unknown[]): void {
@@ -14,9 +10,26 @@ class MyLogger implements Logger {
   }
 }
 
-const ProcessEnv = DbEnv.parse(process.env);
+const DbEnv = z.object({
+  RDS_ENDPOINT: z.string(),
+  RDS_IAM_USER: z.string(),
+  RDS_REGION: z.string(),
+  DATABASE_NAME: z.string(),
+});
 
-const connection = mysql.createPool(ProcessEnv.MYSQL_URL);
+export const ProcessEnv = DbEnv.parse(process.env);
+
+const iamToken = await generateIamToken({
+  host: ProcessEnv.RDS_ENDPOINT,
+  user: ProcessEnv.RDS_IAM_USER,
+  region: ProcessEnv.RDS_REGION,
+});
+const connection = await connectWithIamToken({
+  host: ProcessEnv.RDS_ENDPOINT,
+  user: ProcessEnv.RDS_IAM_USER,
+  token: iamToken,
+  database: ProcessEnv.DATABASE_NAME,
+});
 
 const db = drizzle(connection, { logger: new MyLogger() });
 
