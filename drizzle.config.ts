80991cb73d121b4845cedd5d68ebc64a533949ae
sam <sam@meech-ward.me>
2024-11-07 00:33:22 -0800

    idk

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,30 +1,34 @@
-import { defineConfig } from 'drizzle-kit'
-import { z } from 'zod'
-import { connectWithIamToken, generateIamToken } from './server/db/iam-connect';
-const DbEnv = z.object({
-  RDS_ENDPOINT: z.string(),
-  RDS_IAM_USER: z.string(),
-  RDS_REGION: z.string(),
-  DATABASE_NAME: z.string(),
-});
+import { defineConfig } from "drizzle-kit";
+import { z } from "zod";
+import { generateIamToken } from "./server/db/iam-connect";
 
-export const ProcessEnv = DbEnv.parse(process.env);
+/** @type { import("drizzle-kit").Config } */
+export default (async function () {
+  const DbEnv = z.object({
+    RDS_ENDPOINT: z.string(),
+    RDS_IAM_USER: z.string(),
+    RDS_REGION: z.string(),
+    DATABASE_NAME: z.string(),
+  });
 
-const iamToken = await generateIamToken({
-  host: ProcessEnv.RDS_ENDPOINT,
-  user: ProcessEnv.RDS_IAM_USER,
-  region: ProcessEnv.RDS_REGION,
-});
+  const ProcessEnv = DbEnv.parse(process.env);
 
-export default defineConfig({
-  schema: "./server/db/schema-mysql/*",
-  dialect: 'mysql',
-  dbCredentials: {
+  const iamToken = await generateIamToken({
     host: ProcessEnv.RDS_ENDPOINT,
     user: ProcessEnv.RDS_IAM_USER,
-    password: iamToken,
-    database: ProcessEnv.DATABASE_NAME,
-  },
-  verbose: true,
-  strict: true,
-})
+    region: ProcessEnv.RDS_REGION,
+  });
+
+  return defineConfig({
+    schema: "./server/db/schema-mysql/*",
+    dialect: "mysql",
+    dbCredentials: {
+      host: ProcessEnv.RDS_ENDPOINT,
+      user: ProcessEnv.RDS_IAM_USER,
+      password: iamToken,
+      database: ProcessEnv.DATABASE_NAME,
+    },
+    verbose: true,
+    strict: true,
+  });
+})();
